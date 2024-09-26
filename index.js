38fdb119ad7c6363495c227f99582e85c0c4a329
jwhazel <jwhazel@me.com>
2024-09-26 17:42:32 -0400

    stash

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+const server = http.createServer(async (req, res) => {
+  const ts = await getTimestamp();
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(`Hello World\nThe time is currently: ${ts}`);
 });
 
+const getTimestamp = async () => {
+  const { dateTime } = await fetch(
+    "https://timeapi.io/api/time/current/zone?timeZone=America%2FChicago"
+  ).then((res) => res.json());
+  return dateTime;
+};
+
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
